0cbdc5f03c1c42c63cbf35bc28f80d9d166e27c9
angelanappe <angela.nappe@gmail.com>
2024-03-27 22:22:27 -0300

    JS y CSS para nav-items y botón Enviar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,20 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+//js para cambiar el bg-color de elementos de la nav cuando se clickea
+$(document).ready(function(){
+    $('.nav-link').on('click', function(){
+        $('.nav-item').removeClass('active'); //"activa" 
+        $(this).parent().addClass('active');
+        // permanece 1 segundo con bg-info y recupera fondo inicial
+        setTimeout(function(){
+            $('.nav-item').removeClass('active');
+        }, 1000);
+    });
+});
+
+// js para el botón Enviar
+var boton = document.getElementById('boton-enviar');
+boton.addEventListener('click', function() {
+    alert('Gracias por escribirnos :)');
+});
