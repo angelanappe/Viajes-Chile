8e89b636cdb0dcfb66db0af2829f89373733cea7
angelanappe <angela.nappe@gmail.com>
2024-03-27 22:32:21 -0300

    Agrega alerts(redes sociales) y cambio color cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,3 +19,34 @@ var boton = document.getElementById('boton-enviar');
 boton.addEventListener('click', function() {
     alert('Gracias por escribirnos :)');
 });
+
+// js para crear alertas al dirigirse a otras páginas en los <i> del footer
+document.addEventListener('DOMContentLoaded', function() {
+    // traer todos los <i> del footer
+    var iconos = document.querySelectorAll('footer a i');
+    // darles el 'click'
+    iconos.forEach(function(icono) {
+        icono.addEventListener('click', function(event) {
+            event.preventDefault();
+            // pop up notificando que cambiarás de página
+            var confirmacion = confirm("Estás a punto de abandonar esta página. ¿Deseas continuar?");
+            // redirigir al link si se presiona aceptar
+            if (confirmacion) {
+                window.location.href = this.parentElement.getAttribute('href');
+            }
+        });
+    });
+});
+
+//js para invertir colores en las cards al hacer click sobre ellas
+document.addEventListener('DOMContentLoaded', function() {
+    var cards = document.querySelectorAll('.card'); //trae todas las .card
+
+    cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            this.classList.toggle('bg-info');
+            this.classList.toggle('text-light');
+            this.classList.toggle('bg-white');
+        });
+    });
+});
