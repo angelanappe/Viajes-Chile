0562ff396a77f3364e5a5647fd0437a3c77c094d
angelanappe <angela.nappe@gmail.com>
2024-03-27 21:56:40 -0300

    Tooltips en footer y sección presentación

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+//para habilitar tooltips en footer y en sección presentación
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
